dcd693b9013237563e8576ffe47d6d9ef03c2fa4
katya-petrova <walki-disi@ya.ru>
2021-05-07 01:14:53 +0300

    add slider and change some pages logic

--- script.js
+++ script.js
@@ -317,35 +317,250 @@ let count1 = 0;
       crslLine.children[rangeValue].classList.add('active-descr');
       crslLine.children[rangeValue +1].classList.remove('active-descr');
     }
-    // if (rangeValue === 2 && itemsOnLeft === 8) {
-    //   offset = offset - (crslLine.clientWidth / slidesToShow);
-    //   crslLine.children[rangeValue].classList.add('active-descr');
-    //   crslLine.children[rangeValue +1].classList.remove('active-descr');
-    // }
   });
 
-  //slides[this.count].classList.add('active');
-  //PetsinZooRange.value = count1;
-//console.log(PetsinZooRange.value)
+  //testimonials
 
- //crslElements.forEach((elem) => {
-//    elem.style.minWidth = `${itemWidth}px`
-//  });
+  $('.feedback-slider-wrapper').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
-//  rightArrow.addEventListener('click', function() {
-//     const itemsLeft = itemsCount - (Math.abs(offset) + slidesToShow * itemWidth) / itemWidth;
-//     offset -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-//     setPosition();
-//    // checkBtn();
-//   });
+  // const feedbacks = document.querySelectorAll(
+  //   ".feedback"
+  // );
+  // const feedbackLine = document.querySelector(".feedback-line");
+  // let count2 = 0;
+  // let width2;
+  
+  // function init2() {
+  //   if (window.innerWidth > 1300) {
+  //   width2 = document.querySelector(".line-wrap").offsetWidth;
+  //   feedbackLine.style.width2 = width2 * feedbacks.length + "px";
+  //   feedbacks.forEach((item) => {
+  //     item.style.width = width2 + "px"; 
+  //   });
+  //   rollSlider2();
+  // }
+  // }
+  
+  // /*сохраняет пропрорции, но получаетя не так, как в макете, если убрать это событие, то будет как в макете, то есть изображение 
+  // будет постепенно обрезаться, высота останется изначальной*/
+  // window.addEventListener("resize", init2);
+  // init2();
+  
+  // const feedbackRange = document.querySelector(".testimonials-range input");
+  
+  // feedbackRange.addEventListener("input", function () {
+  //   for (let i = 0; i < feedbacks.length; i++) {
+  //     count2 = feedbackRange.value - 1;
+  //     rollSlider2()
+  //   }
+  // });
+  
+  // function rollSlider2() {
+  //   feedbackLine.style.transform = "translate(-" + count2 * width2 + "px)";
+  // }
 
-//   leftArrow.addEventListener('click', function() {
-//     const itemsLeft = itemsCount - Math.abs(offset) / itemWidth;
-//     offset += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-//     setPosition();
-//     checkBtn();
-//   });
+  //-----attempt to make slider using class-----//
 
-//   const setPosition = () => {
-//     crslLine.style.transform = `translateX(${offset}px)`;
-//   };
+  // function Ant(crslId) {
+
+  //   let id = document.getElementById(crslId);
+  //   if(id) {
+  //     this.crslRoot = id
+  //   }
+  //   else {
+  //     this.crslRoot = document.querySelector('.feedback-slider-wrapper')
+  //   };
+  
+  //   // Carousel objects
+  //   this.crslList = this.crslRoot.querySelector('.feedback-line');
+  //   this.crslElements = this.crslList.querySelectorAll('.feedback');
+  //   this.crslElemFirst = this.crslList.querySelector('.feedback');
+  //   this.leftArrow = this.crslRoot.querySelector('.feedback-slider-item .left-arrow');
+  //   this.rightArrow = this.crslRoot.querySelector('.feedback-slider-item .right-arrow');
+  //   this.feedbackRange = this.crslRoot.querySelector('.estimonials-range input');
+
+  
+  //   // Initialization
+  //   this.options = Ant.defaults;
+  //   Ant.initialize(this)
+  // };
+  
+  // Ant.defaults = {
+  
+  //   // Default options for the carousel
+  //   elemVisible: 8, // Кол-во отображаемых элементов в карусели
+  //   loop: true,     // Бесконечное зацикливание карусели 
+  //   auto: false,     // Автоматическая прокрутка
+  //   interval: 5000, // Интервал между прокруткой элементов (мс)
+  //   speed: 750,     // Скорость анимации (мс)
+  //   touch: true,    // Прокрутка  прикосновением
+  //   arrows: true,   // Прокрутка стрелками
+  //   dots: false      // Индикаторные точки
+  // };
+  
+  // Ant.prototype.elemPrev = function(num) {
+  //   num = num || 1;
+  
+  //   if(!this.options.loop) {  // сдвиг вправо без цикла
+  //     this.currentOffset += this.elemWidth*num;
+  //     this.crslList.style.marginLeft = this.currentOffset + 'px';
+  //     if(this.currentElement == 0) {
+  //       this.leftArrow.style.display = 'none'; this.touchPrev = false
+  //     }
+  //     this.rightArrow.style.display = 'block'; this.touchNext = true
+  //   }
+  //   else {                    // сдвиг вправо с циклом
+  //     let elm, buf, this$ = this;
+  //     for(let i=0; i<num; i++) {
+  //       elm = this.crslList.lastElementChild;
+  //       buf = elm.cloneNode(true);
+  //       this.crslList.insertBefore(buf, this.crslList.firstElementChild);
+  //       this.crslList.removeChild(elm)
+  //     }; 
+  //     this.crslList.style.marginLeft = '-' + this.elemWidth*num + 'px';
+  //     let compStyle = window.getComputedStyle(this.crslList).marginLeft;
+  //     this.crslList.style.cssText = 'transition:margin '+this.options.speed+'ms ease;';
+  //     this.crslList.style.marginLeft = '0px';
+  //     setTimeout(function() {
+  //       this$.crslList.style.cssText = 'transition:none;'
+  //     }, this.options.speed)
+  //   }
+  // };
+  
+  // Ant.prototype.elemNext = function(num) {
+  //   num = num || 1;
+  
+  //   if(!this.options.loop) {  // сдвиг влево без цикла
+  //     this.currentOffset -= this.elemWidth*num;
+  //     this.crslList.style.marginLeft = this.currentOffset + 'px';
+  //      console.log(this.currentElement);
+  //     if(this.currentElement == this.dotsVisible-1) {
+  //       this.rightArrow.style.display = 'none'; this.touchNext = false
+  //     }
+  //     this.leftArrow.style.display = 'block'; this.touchPrev = true
+  //   }
+  //   else {                    // сдвиг влево с циклом
+  //     let elm, buf, this$ = this;
+  //     this.crslList.style.cssText = 'transition:margin '+this.options.speed+'ms ease;';
+  //     this.crslList.style.marginLeft = '-' + this.elemWidth*num + 'px';
+  //     setTimeout(function() {
+  //       this$.crslList.style.cssText = 'transition:none;';
+  //       for(let i=0; i<num; i++) {
+  //         elm = this$.crslList.firstElementChild;
+  //         buf = elm.cloneNode(true); this$.crslList.appendChild(buf);
+  //         this$.crslList.removeChild(elm)
+  //       };
+  //       this$.crslList.style.marginLeft = '0px'
+  //     }, this.options.speed)
+  //   }
+  // };
+  
+  // Ant.initialize = function(that) {
+  
+  //   // Constants
+  //   that.elemCount = that.crslElements.length; // Количество элементов
+  // //  that.dotsVisible = that.elemCount;         // Число видимых точек
+  //   let elemStyle = window.getComputedStyle(that.crslElemFirst);
+  
+  //   that.elemWidth = that.crslElemFirst.offsetWidth +  // Ширина элемента (без margin)
+  //     parseInt(elemStyle.marginLeft) + parseInt(elemStyle.marginRight);
+  
+  //   // Variables
+  //   that.currentElement = 0; that.currentOffset = 0;
+  //   that.touchPrev = true; that.touchNext = true;
+  //   let xTouch, yTouch, xDiff, yDiff, stTime, mvTime;
+  //   let bgTime = getTime();
+  
+  //   // Functions
+  //   function getTime() {
+  //     return new Date().getTime();
+  //   };
+  
+  //   if(that.options.touch) {   // инициализация прокрутки прикосновением
+  //     that.crslList.addEventListener('touchstart', function(e) {
+  //       xTouch = parseInt(e.touches[0].clientX);
+  //       yTouch = parseInt(e.touches[0].clientY);
+  //       stTime = getTime()
+  //     }, false);
+  //     that.crslList.addEventListener('touchmove', function(e) {
+  //       if(!xTouch || !yTouch) return;
+  //       xDiff = xTouch - parseInt(e.touches[0].clientX);
+  //       yDiff = yTouch - parseInt(e.touches[0].clientY);
+  //       mvTime = getTime();
+  //       if(Math.abs(xDiff) > 15 && Math.abs(xDiff) > Math.abs(yDiff) && mvTime - stTime < 75) {
+  //         stTime = 0;
+  //         if(that.touchNext && xDiff > 0) {
+  //           bgTime = mvTime; that.elemNext()
+  //         }
+  //         else if(that.touchPrev && xDiff < 0) {
+  //           bgTime = mvTime; that.elemPrev()
+  //         }
+  //       }
+  //     }, false)
+  //   };
+  
+  //   if(that.options.arrows) {  // инициализация стрелок
+  //     if(!that.options.loop) that.crslList.style.cssText =
+  //       'transition:margin '+that.options.speed+'ms ease;';
+  //     that.leftArrow.addEventListener('click', function() {
+  //       let fnTime = getTime();
+  //       if(fnTime - bgTime > that.options.speed) {
+  //         bgTime = fnTime; that.elemPrev()
+  //       }
+  //     }, false);
+  //     that.rightArrow.addEventListener('click', function() {
+  //       let fnTime = getTime();
+  //       if(fnTime - bgTime > that.options.speed) {
+  //         bgTime = fnTime; that.elemNext()
+  //       }
+  //     }, false)
+  //   }
+  // };
+  
+  // new Ant();
+  
+  
+  
